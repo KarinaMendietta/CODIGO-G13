28292576e0c7268ae7d44ab2cae36a40101d2d41
KarinaMendietta <karinamendiettasoto@gmail.com>
2022-04-22 22:59:27 -0500

    dia04 y dia 05

--- semana02/dia05/app/portafolio/main.py
+++ semana02/dia05/app/portafolio/main.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request,session
+#import requests
 
-from . import portafolio
+#URL = 'https://api.github.com/users/cesarmayta'
+
 
 ## para conectarse a firebase
 import firebase_admin
@@ -14,9 +16,15 @@ from firebase_admin import firestore
 
 db = firestore.client()
 
+app = Flask(__name__)
 
-@portafolio.route('/')
-def home():
+#creamos una clave secreta para las variables de sesión
+app.secret_key = 'qwerty123456'
+
+@app.route('/')
+def index():
+    #return '<center><H1>BIENVENIDO A MI SITIO WEB</H1></center>'
+    #recuperamos la info de la biografia
     colBiografia = db.collection('biografia')
     docBiografia = colBiografia.get()
 
@@ -35,15 +43,35 @@ def home():
         lstEnlaces.append(dicEnlace)
 
     session['enlaces'] = lstEnlaces
-    return render_template('portafolio/home.html')
+
+
+    #data = requests.get(URL)
+
+    #nombre = request.args.get('nombre','')
+    #context = data.json()
+    #print(context)
+
+    return render_template('home.html')
+
+@app.route('/peliculas')
+def peliculas():
+    listaPeliculas = ['CODA','ENCANTO','SONIC 2']
+
+    nombre = request.args.get('nombre','')
+
+    context = {
+        'nombre':nombre,
+        'peliculas':listaPeliculas
+    }
+    return render_template('peliculas.html',**context)
 
 ############### RUTAS DE MIS PAGINAS
-@portafolio.route('/acercade')
+@app.route('/acercade')
 def about():
-    return render_template('portafolio/acercade.html')
+    return render_template('acercade.html')
 
-@portafolio.route('/proyectos')
-def proyectos():
+@app.route('/portafolio')
+def portafolio():
     colProyectos = db.collection('proyectos')
     docProyectos = colProyectos.get()
 
@@ -57,8 +85,10 @@ def proyectos():
     context = {
         'proyectos':lstProyectos
     }
-    return render_template('portafolio/portafolio.html',**context)
+    return render_template('portafolio.html',**context)
 
-@portafolio.route('/contacto')
+@app.route('/contacto')
 def contacto():
-    return render_template('portafolio/contacto.html')
+    return render_template('contacto.html')
+
+app.run(debug=True)
